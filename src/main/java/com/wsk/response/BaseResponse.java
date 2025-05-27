5196af17677d2d7833c5cb17c1269d626b5a8390
githubkzw <13737772535@163.com>
2025-05-27 10:48:34 +0800

    1.注册页重叠修复 2.忘记密码功能完善(1.1)_2025_05_27

--- src/main/java/com/wsk/response/BaseResponse.java
+++ src/main/java/com/wsk/response/BaseResponse.java
@@ -12,35 +12,37 @@ import lombok.Data;
 @Data
 public class BaseResponse {
     private int result;
-
     private String msg;
+    private boolean success;
 
-    public BaseResponse(int result, String msg) {
+    public BaseResponse(int result, String msg, boolean success) {
         this.result = result;
         this.msg = msg;
+        this.success = success;
     }
 
     public BaseResponse(int result) {
         this.result = result;
+        this.success = result == 1;
     }
 
     public static BaseResponse fail() {
-        return new BaseResponse(0, GlobalExceptionHandler.DEFAULT_ERROR_MESSAGE);
+        return new BaseResponse(0, GlobalExceptionHandler.DEFAULT_ERROR_MESSAGE, false);
     }
 
     public static BaseResponse fail(String msg) {
-        return new BaseResponse(0, msg);
+        return new BaseResponse(0, msg, false);
     }
 
     public static BaseResponse fail(int result) {
-        return new BaseResponse(result);
+        return new BaseResponse(result, GlobalExceptionHandler.DEFAULT_ERROR_MESSAGE, false);
     }
 
     public static BaseResponse success() {
-        return new BaseResponse(1, "success");
+        return new BaseResponse(1, "success", true);
     }
 
     public static BaseResponse success(String msg) {
-        return new BaseResponse(1, msg);
+        return new BaseResponse(1, msg, true);
     }
 }
--- src/main/resources/mystatic/js/login/init_login.js
+++ src/main/resources/mystatic/js/login/init_login.js
@@ -2,24 +2,53 @@
  * Created by kzw on 2025/5/9.
  */
 $(function () {
+    // 定义css3Transform函数
+    function css3Transform(element, value) {
+        var arrPriex = ["transform", "webkitTransform", "mozTransform", "msTransform", "oTransform"];
+        for(var i = 0; i < arrPriex.length; i++) {
+            element.style[arrPriex[i]] = value;
+        }
+    }
+    
     //1 注册时忘记密码
     var from_which = 0;
-//            !!!!!旋转操作无论如何根据都是根据开始位置！！！
-    $('.enter_password').hide(0);
+    
+    // 初始化时隐藏不需要的页面
+    $('.enter_password').hide();
+    $('.register_page').hide();
+    $('.forget_password').hide();
+
+    // 点击注册按钮
     $('.flip_to_register').bind('click', function () {
-        $('.enter_password').show(500);
-        css3Transform(document.getElementsByClassName('content')[0], "rotateY(-90deg)");
-        $('.forget_password').hide(500);
-        refreshVerificationCode('register'); // 添加验证码刷新
-    });
-    $('.go_to_forget').bind('click', function () {
-        $('.enter_password').show(500);
-        css3Transform(document.getElementsByClassName('content')[0], "rotateY(90deg)");
-        $('.register_page').hide(500);
-        refreshVerificationCode('forget'); // 添加验证码刷新
+        $('.login').fadeOut(300, function() {
+            $('.register_page').fadeIn(300);
+            css3Transform(document.getElementsByClassName('content')[0], "rotateY(-90deg)");
+        });
+        $('.forget_password').hide();
+        $('.enter_password').hide();
+        refreshVerificationCode('register');
     });
 
-    // ... 保持原有的返回登录等动画代码 ...
+    // 点击忘记密码
+    $('.go_to_forget').bind('click', function () {
+        $('.login').fadeOut(300, function() {
+            $('.forget_password').fadeIn(300);
+            css3Transform(document.getElementsByClassName('content')[0], "rotateY(90deg)");
+        });
+        $('.register_page').hide();
+        $('.enter_password').hide();
+        refreshVerificationCode('forget');
+    });
+
+    // 返回登录
+    $('.go_back_login, .go_back_login_from_forget').bind('click', function () {
+        $('.register_page').fadeOut(300);
+        $('.forget_password').fadeOut(300);
+        $('.enter_password').fadeOut(300, function() {
+            $('.login').fadeIn(300);
+            css3Transform(document.getElementsByClassName('content')[0], "rotateY(0deg)");
+        });
+    });
 
     // 跳转到注册输入密码
     $('.go_enter_password_button').bind('click', function () {
@@ -55,12 +84,17 @@ $(function () {
                 var result = data.result;
                 if (result === 0) {
                     alert('验证码错误');
-                    refreshVerificationCode('register'); // 验证码错误时刷新
+                    refreshVerificationCode('register');
+                } else if (result === -1) {
+                    alert('手机号已被注册');
+                    refreshVerificationCode('register');
                 } else if (result === 1) {
-                    // 成功后执行以下代码进行跳转
-                    $('.forget_password').show(1000);
-                    css3Transform(document.getElementsByClassName('content')[0], "rotateY(-180deg)");
-                    $('.login').hide(1000);
+                    $('.register_page').fadeOut(300);
+                    $('.login').hide();
+                    $('.forget_password').hide();
+                    $('.enter_password').fadeIn(300, function() {
+                        css3Transform(document.getElementsByClassName('content')[0], "rotateY(-180deg)");
+                    });
                     $('.confirm_register_button').html('注册');
                 }
             },
@@ -71,11 +105,8 @@ $(function () {
         });
     });
 
-    // ... 保持原有的密码确认和重置密码逻辑 ...
-
-    // 跳转到重置密码输入
+    // 忘记密码按钮
     $('.forget_password_button').bind('click', function () {
-        from_which = 2;
         var phone = $('.forget_input_phone').val();
         var code = $('.forget_input_vcode').val();
         var token = $('.token').val();
@@ -102,12 +133,14 @@ $(function () {
                 var result = data.result;
                 if (result === 0) {
                     alert('验证码错误');
-                    refreshVerificationCode('forget'); // 验证码错误时刷新
+                    refreshVerificationCode('forget');
                 } else if (result === 1) {
-                    // 成功后执行以下代码进行跳转
-                    $('.register_page').show(1000);
-                    css3Transform(document.getElementsByClassName('content')[0], "rotateY(180deg)");
-                    $('.login').hide(1000);
+                    $('.forget_password').fadeOut(300);
+                    $('.login').hide();
+                    $('.register_page').hide();
+                    $('.enter_password').fadeIn(300, function() {
+                        css3Transform(document.getElementsByClassName('content')[0], "rotateY(180deg)");
+                    });
                     $('.confirm_register_button').html('重置密码');
                 }
             },
@@ -118,9 +151,61 @@ $(function () {
         });
     });
 
-    // ... 保持原有的所有动画相关代码...
+    // 注册/重置密码按钮点击事件
+    $('.confirm_register_button').bind('click', function() {
+        var password = $('.first_enter_password_input').val();
+        var confirmPassword = $('.confirm_password_input').val();
+        var token = $('.token').val();
 
-    // 添加验证码刷新函数
+        if (password === '') {
+            alert('密码不能为空');
+            return;
+        }
+        if (confirmPassword === '') {
+            alert('确认密码不能为空');
+            return;
+        }
+        if (password !== confirmPassword) {
+            alert('两次输入的密码不一致');
+            return;
+        }
+        if (password.length < 6) {
+            alert('密码长度不能少于6位');
+            return;
+        }
+
+        // 根据按钮文本判断是注册还是重置密码
+        var isReset = $(this).html() === '重置密码';
+        
+        $.ajax({
+            url: isReset ? 'updatePassword.do' : 'insertUser.do',
+            type: 'post',
+            dataType: 'JSON',
+            data: {
+                password: password,
+                token: token
+            },
+            success: function(data) {
+                if (data.success) {
+                    alert(isReset ? '密码重置成功！' : '注册成功！');
+                    window.location.href = '/';
+                } else {
+                    if (data.message) {
+                        alert(data.message);
+                    } else {
+                        alert(isReset ? '密码重置失败，请重试' : '注册失败，请重试');
+                    }
+                    refreshVerificationCode(isReset ? 'forget' : 'register');
+                }
+            },
+            error: function() {
+                alert('服务器错误，请重试');
+                refreshVerificationCode(isReset ? 'forget' : 'register');
+            }
+        });
+    });
+
+    // 验证码刷新函数
     function refreshVerificationCode(type) {
         $.get('/getVerificationCode.do', function(data) {
             if(type === 'register') {
